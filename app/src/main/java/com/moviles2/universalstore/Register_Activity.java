24f6c6d14456c3335e20b10fefe40010bfc9bca3
York Mosquera <yolemos_10@hotmail.com>
2021-10-03 14:41:14 -0500

    configuro el boton atras y el tema oscuro

--- app/src/main/java/com/moviles2/universalstore/Register_Activity.java
+++ app/src/main/java/com/moviles2/universalstore/Register_Activity.java
@@ -2,11 +2,16 @@ package com.moviles2.universalstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Register_Activity extends AppCompatActivity {
+
+    Button btnatras;
 
     Spinner spinner;
 
@@ -15,9 +20,16 @@ public class Register_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        btnatras = findViewById(R.id.btnatras);
+
         spinner = findViewById(R.id.spinner);
         String[] opciones = {" ","Usuario","Vendedor"};
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item_rol, opciones);
         spinner.setAdapter(adapter);
+    }
+
+    public void atras(View view){
+        Intent atras = new Intent(this, activity_login.class);
+        startActivity(atras);
     }
 }
